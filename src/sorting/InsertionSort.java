5999b6a6081ead6e12fe87a76a871e1c7da76323
Abhishek Jadon <jadonabhi0@gmail.com>
2024-04-19 23:01:11 +0530

    Insertion sort

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -1,2 +1,30 @@
-package sorting;public class InsertionSort {
+package sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    // TC ->
+    // Best case -> O(N)
+    // Average and Worst => O(N^2)
+
+    public static int[] insertionSort(int[] arr){
+        int n = arr.length;
+        for(int i = 1 ; i < n ; i++){
+            int num = arr[i];
+            int j = i-1; // pointer for shifting element
+            while (j >= 0 && arr[j] > num) arr[j+1] = arr[j--]; // shifting element
+            arr[j+1] = num;
+        }
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{2,6,4,21,1,1,8,9,3};
+        int[] sort = insertionSort(arr);
+        System.out.println(Arrays.toString(sort));
+    }
+
 }
