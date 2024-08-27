0fe73747493c1d62dc8becaaa031dac8690a43b3
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2024-08-27 22:43:40 +0530

    Update MatrixSnakeTraversa

--- src/array/MatrixSnakeTraversal.java
+++ src/array/MatrixSnakeTraversal.java
@@ -9,7 +9,6 @@ public class MatrixSnakeTraversal {
                 {11, 12, 13, 14, 15},
                 {16, 17, 18, 19, 20},
                 {21, 22, 23, 24, 25}};
-
         print(mat);
 
     }
